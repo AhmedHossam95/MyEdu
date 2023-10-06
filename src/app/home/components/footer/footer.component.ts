5075cf6125ea2d4ec201c8ebd6a7b2de31829679
ahmedhossam95 <ahmed_hossam_eldeen@outlook.com>
2023-10-06 15:08:16 +0300

    added footer component links && enhanced scss classes

--- src/app/home/components/footer/footer.component.ts
+++ src/app/home/components/footer/footer.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { LOGO_BLACK_PATH } from 'src/app/core/constants/imgs-paths';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent  implements OnInit {
-
+export class FooterComponent implements OnInit {
+  logoPath!: string;
+  links: string[] = [];
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getLogoPath();
+    this.getLinks();
+  }
 
+  getLogoPath(): void {
+    this.logoPath = LOGO_BLACK_PATH;
+  }
+
+  getLinks(): void {
+    this.links = ['link one', 'link two', 'link three', 'link four', 'link five'];
+  }
 }
