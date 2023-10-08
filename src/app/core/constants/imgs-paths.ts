941a107d89666a427b0d681b092003062f33af95
ahmedhossam95 <ahmed_hossam_eldeen@outlook.com>
2023-10-08 14:46:52 +0300

    Auto-generated commit [ci skip]

--- src/app/core/constants/imgs-paths.ts
+++
@@ -1,8 +0,0 @@
-export const LOGO_PATH = 'assets/imgs/logo.svg';
-export const LOGO_BLACK_PATH = 'assets/imgs/logo-black.svg';
-export const MAIN_CONTENT_IMG_PATH = '../../../../assets/imgs/main-content-img.png';
-export const STEPS_IMGS = {
-    SUBSCRIBE_IMG_PATH: '../../../../assets/imgs/subscribe.svg',
-    CUSTOMIZE_IMG_PATH: '../../../../assets/imgs/customize.svg',
-    SURPRISE_IMG_PATH: '../../../../assets/imgs/surprise.svg',
-}
